1378da148ca99ca7c70f209fb1e9816d104580c9
Yoo isae <dbdlto@naver.com>
2020-04-14 22:44:44 +0900

    add find in main.c

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int selectmenu(){
         printf("4. 제품삭제\n");
 	printf("5. 저장\n");
         printf("6. 저장된 파일 가져오기\n");
+        printf("7. 검색\n");
         printf("0. 종료\n");
         printf("==>원하는 메뉴를 선택하세요.");
         scanf("%d", &menu);
@@ -122,6 +123,18 @@ int main(){
                                         count=loadFile(p);
                                         culcount=count;
                                         printf("==로딩성공==\n");
+                        }
+                }
+                else if(menu==7){
+                        if(count==0){
+                                printf("==데이터가 없습니다.==\n");
+                                continue;
+                        }
+                        else{
+                                #ifdef DEBUG
+                                        printf("debug[%s]: %d %s\n", __FILE__, __LINE__, __FUNCTION__);
+                                #endif
+                                        find(p, culcount);
                         }
                 }
 	}
